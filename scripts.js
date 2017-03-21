3a0819ab5361aa4581b6f45c2679407fd7624d6b
Andrew O'Rourke <andrew.john.orourke@gmail.com>
2017-03-22 01:57:19 +1000

    Fixed a lot of small things, have a pretty cool idea for a secondary navigation on scroll. but too sleepy.

---
+++ scripts.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+	// $(window).scroll(function() {
+	// 	if ($(this).scrollTop() > 1){  
+	// 	    $('nav').addClass("nav-sticky");
+	// 	}
+	// 	else{
+	// 	    $('nav').removeClass("nav-sticky");
+	// 	  }
+	// });
+
+	$(document).on('click', '#about-me-link, #portfolio-link, #contact-button', function(event){
+	    event.preventDefault();
+
+	    $('html, body').animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top
+	    }, 500);
+	});
+});
+
