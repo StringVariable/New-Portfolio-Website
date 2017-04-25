331f19ec0e6311118e1df3f3efed49ea8ab70e27
Andrew O'Rourke <andrew.john.orourke@gmail.com>
2017-04-25 17:03:58 +1000

    Reduced the speed in which the contact me button scrolls to the bottom of the page

--- scripts.js
+++ scripts.js
@@ -1,18 +1,14 @@
 $( document ).ready(function() {
-	// $(window).scroll(function() {
-	// 	if ($(this).scrollTop() > 1){  
-	// 	    $('nav').addClass("nav-sticky");
-	// 	}
-	// 	else{
-	// 	    $('nav').removeClass("nav-sticky");
-	// 	  }
-	// });
-
-	$(document).on('click', '#about-me-link, #portfolio-link, #contact-button', function(event){
+	$(document).on('click', '#about-me-link, #portfolio-link', function(event){
 	    event.preventDefault();
 	    $('html, body').animate({
 	        scrollTop: $( $.attr(this, 'href') ).offset().top
 	    }, 500);
 	});
+	$(document).on('click', '#contact-button', function(event){
+			event.preventDefault();
+			$('html, body').animate({
+					scrollTop: $( $.attr(this, 'href') ).offset().top
+			}, 1500);
+	});
 });
-
